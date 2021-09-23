102fa1fb5c4c9b6a63c7980087788b1cf3f53fbe
Jariah Holsapple <jariah_holsapple@advantagesolutions.net>
2021-09-23 11:52:50 -0400

    Better Styling for widget

--- projects/main/src/app/app.component.ts
+++ projects/main/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'main';
-  color: 'primary' | 'accent' | 'warn' | null = null;
+  color: 'primary' | 'accent' | 'warn' | null = 'primary';
 }
